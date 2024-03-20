f88056fee9b5be3028e49937365e55af03414100
Piyush Shewale <92243361+Piyush-OG@users.noreply.github.com>
2024-03-20 14:58:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/N meetings in one room/n-meetings-in-one-room.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+
+            int start[] = new int[n];
+            int end[] = new int[n];
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++)
+                start[i] = Integer.parseInt(inputLine[i]);
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) 
+                end[i] = Integer.parseInt(inputLine[i]);
+                
+            int ans = new Solution().maxMeetings(start, end, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    public static int maxMeetings(int start[], int end[], int n){
+        int[][] inp=new int[n][2];
+        for(int i=0;i<n;i++){
+            inp[i][0]=start[i];
+            inp[i][1]=end[i];
+        }
+        Arrays.sort(inp,(a,b)->a[1]-b[1]);
+        
+        int curr=inp[0][1];
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(curr>=inp[i][0]){
+                continue;
+            }
+            else{
+                curr=inp[i][1];
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
